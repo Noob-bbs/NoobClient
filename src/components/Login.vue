82cb0567a2896e1cba32b9e2512395415f19badd
haojie06 <haojie06@qq.com>
2020-06-28 11:40:58 +0800

    使用bootstrap-vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,107 +1,122 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="../assets/logo.png" />
     <div>
-      <div v-if="mode == 'login'">
-        账户:
-        <input type="text" v-model="user.account" placeholder="请输入用户名" />
-        密码:
-        <input type="password" v-model="user.password" placeholder="请输入密码" />
-        <button @click="login">登录</button>
-        <button @click="mode = 'register'" class="register-link">注册账户</button>
-        <button @click="logout">登出</button>
-        <button @click="me">me</button>
-      </div>
-      <div v-else-if="mode == 'register'">
-        账户:
-        <input type="text" v-model="user.account" placeholder="请输入用户名" />
-        密码:
-        <input type="password" v-model="user.password" placeholder="请输入密码" />
-        邮箱:
-        <input type="email" v-model="user.email" placeholder="请输入邮箱" />
-        <button @click="register">注册</button>
-        <button @click="mode == 'login'">登录</button>
-      </div>
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <div>
+            <div v-if="mode == 'login'">
+                账户:
+                <input type="text" v-model="user.account" placeholder="请输入用户名" />
+                密码:
+                <input
+                    type="password"
+                    v-model="user.password"
+                    placeholder="请输入密码"
+                />
+                <button @click="login">登录</button>
+                <button @click="mode = 'register'" class="register-link">注册账户</button>
+                <button @click="logout">登出</button>
+                <button @click="me">me</button>
+            </div>
+            <div v-else-if="mode == 'register'">
+                账户:
+                <input type="text" v-model="user.account" placeholder="请输入用户名" />
+                密码:
+                <input
+                    type="password"
+                    v-model="user.password"
+                    placeholder="请输入密码"
+                />
+                邮箱:
+                <input type="email" v-model="user.email" placeholder="请输入邮箱" />
+                <button @click="register">注册</button>
+                <button @click="mode == 'login'">登录</button>
+            </div>
+            <Card></Card>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
+import Card from './home/Card.vue' 
 export default {
-  name: "Login",
-  data() {
-    return {
-      user: {
-        account: "",
-        password: ""
-      },
-      responseResult: [],
-      mode: "login"
-    };
-  },
-  methods: {
-    login() {
-      this.$axios
-        .post("/login", {
-          account: this.user.account,
-          password: this.user.password
-        })
-        .then(successResponse => {
-          console.log("成功发送请求" + JSON.stringify(successResponse));
-          if (successResponse.data.statusCode === 200) {
-            this.$router.replace({ path: "/index" });
-            console.log("登陆成功");
-          }
-        })
-        .catch(failReponse => {
-          console.log("login error" + failReponse);
-        });
+    components: {Card},
+    name: "Login",
+    data() {
+        return {
+            user: {
+                account: "",
+                password: ""
+            },
+            responseResult: [],
+            mode: "login"
+        };
     },
-    register() {
-      this.$axios
-        .post("/register", {
-          account: this.user.account,
-          password: this.user.password,
-          email: this.user.email
-        })
-        .then(successResponse => {
-          console.log("成功发送请求" + JSON.stringify(successResponse));
-          if (successResponse.data.statusCode === 200) {
-            this.mode = "login";
-            console.log("注册成功");
-          }
-        })
-        .catch(failReponse => {
-          console.log("注册错误" + failReponse);
-        });
-    },
-    logout() {
-      this.$axios
-        .get("/logout")
-        .then(response => {
-          console.log("成功发送请求" + JSON.stringify(response));
-        })
-        .catch(failReponse => {
-          console.log("错误" + failReponse);
-        });
-    },
-    me() {
-      this.$axios
-        .get("/me")
-        .then(response => {
-          console.log("成功发送请求" + JSON.stringify(response));
-        })
-        .catch(failReponse => {
-          console.log("错误" + failReponse);
-        });
+    methods: {
+        login() {
+            this.$axios
+                .post("/login", {
+                    account: this.user.account,
+                    password: this.user.password
+                })
+                .then(successResponse => {
+                    console.log(
+                        "成功发送请求" + JSON.stringify(successResponse)
+                    );
+                    if (successResponse.data.statusCode === 200) {
+                        this.$router.replace({ path: "/index" });
+                        console.log("登陆成功");
+                    }
+                })
+                .catch(failReponse => {
+                    console.log("login error" + failReponse);
+                });
+        },
+        register() {
+            this.$axios
+                .post("/register", {
+                    account: this.user.account,
+                    password: this.user.password,
+                    email: this.user.email
+                })
+                .then(successResponse => {
+                    console.log(
+                        "成功发送请求" + JSON.stringify(successResponse)
+                    );
+                    if (successResponse.data.statusCode === 200) {
+                        this.mode = "login";
+                        console.log("注册成功");
+                    }
+                })
+                .catch(failReponse => {
+                    console.log("注册错误" + failReponse);
+                });
+        },
+        logout() {
+            this.$axios
+                .get("/logout")
+                .then(response => {
+                    console.log("成功发送请求" + JSON.stringify(response));
+                })
+                .catch(failReponse => {
+                    console.log("错误" + failReponse);
+                });
+        },
+        me() {
+            this.$axios
+                .get("/me")
+                .then(response => {
+                    console.log("成功发送请求" + JSON.stringify(response));
+                })
+                .catch(failReponse => {
+                    console.log("错误" + failReponse);
+                });
+        }
     }
-  }
 };
 </script>
 
 <style scoped>
 .register-link {
-  margin-top: 20px;
-  left: 0px;
+    margin-top: 20px;
+    left: 0px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 // 设置反向代理，前端请求默认发送到 http://localhost:8443/api
 // 全局注册，之后可在其他组件中通过 this.$axios 发送数据
 //注意修改原型链的用途
 Vue.prototype.$axios = axios
 axios.defaults.baseURL = '/api'
 Vue.config.productionTip = false
-
+Vue.use(BootstrapVue)
 new Vue({
   router,
   render: h => h(App)

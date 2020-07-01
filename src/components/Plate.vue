8d6b9942cc9c7c19c55671f0376ff4f6d3ae3077
PanDaring0 <48867861+PanDaring0@users.noreply.github.com>
2020-07-01 12:07:35 +0800

    Add files via upload

---
+++ src/components/Plate.vue
@@ -0,0 +1,111 @@
+<html>
+<head>
+<script>
+function getNum(data){
+
+}
+</script>
+<style>
+h2.listTitle{color:#005FF0;}
+p.listLink{color:black;}
+sidebar-block
+</style>
+</head>
+
+<body>
+<!--分区-->
+<div id="sidebar_shortcut" class="sidebar-block">
+    <div class = "listLink">
+        <h2 class="listTitle">分区链接</h2>
+        <ul>
+            <li>
+                ::marker
+                <a href="" title="水区列表"水区</a>
+            </li>
+            <li>
+                ::marker
+                <a href="" title="主机交易列表">主机交易</a>
+                <h3>二手小鸡交换中</h3>
+            </li>
+            <li>
+                ::marker
+                <a href="" title="主机信息列表">主机信息</a>
+                <h3>主机优惠信息及AFF集合地</h3>
+            </li>
+            <li>
+                ::marker
+                <a href="" title="影音娱乐列表">影音娱乐</a>
+                <h3>电影游戏软件等</h3>
+            </li>
+            <li>
+                ::marker
+                <a href="" title="友商介绍列表">友商介绍</a>
+            </li>
+            <li>
+                ::marker
+                <a href="" title="公告列表">公告</a>
+            </li>
+        </ul>
+
+        <div id="itemListLin_con" style="display:none;">
+            <ul>
+            </ul>
+        </div>
+
+    </div>
+</div>
+
+<!--论坛数据-->
+<div id="sidebar_shortcut" class="sidebar-block">
+    <div class = "listLink">
+    <h2 class="listTitle">论坛数据</h2>
+    <ul>
+        <p><font size="3">10</font></p>
+        <p><font size="4" color="grey">在线</font></p>
+    </ul>
+
+    <ul>
+        <p><font size="3">10</font></p>
+        <p><font size="4" color="grey">用户</font></p>
+    </ul>
+
+    <ul>
+        <p><font size="3">10</font></p>
+        <p><font size="4" color="grey">主题</font></p>
+    </ul>
+
+    <ul>
+        <p><font size="3">10</font></p>
+        <p><font size="4" color="grey">帖子</font></p>
+    </ul>
+    </div>
+</div>
+
+<!--热门标签-->
+<div id="sidebar_shortcut" class="sidebar-block">
+    <div class = listLink">
+    <h2 class="listTitle">热门标签</h2>
+    <ul>
+        <p><font color="grey">标签--</font><font color:#05FFF0>10</font></p>
+    </ul>
+    <ul>
+        <p><font color="grey">标签--</font><font color:#05FFF0>10</font></p>
+    </ul>
+    <ul>
+        <p><font color="grey">标签--</font><font color:#05FFF0>10</font></p>
+    </ul>
+    <ul>
+        <p><font color="grey">标签--</font><font color:#05FFF0>10</font></p>
+    </ul>
+    <ul>
+        <p><font color="grey">标签--</font><font color:#05FFF0>10</font></p>
+    </ul>
+    <ul>
+        <p><font color="grey">标签--</font><font color:#05FFF0>10</font></p>
+    </ul>
+    </div>
+</div>
+
+</body>
+
+</html>
